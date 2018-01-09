79be5ea12bc6cbdcd642191480be477a1323f5fb
Emma Ramirez <ramirez.emma.g@gmail.com>
2018-01-08 22:22:57 -0700

    chore(core): configure init component

---
+++ packages/core/src/components/Screen/Screen.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export class Screen extends React.Component {
+    constructor(props:any) {
+        super(props);
+    }
+
+    public render() {
+        return <section className='screen'></section>;
+    }
+}
