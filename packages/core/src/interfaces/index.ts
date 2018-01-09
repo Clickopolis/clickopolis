251b91ab15d065f3d7c7018eb5c4eeb73963b0ea
Emma Ramirez <ramirez.emma.g@gmail.com>
2018-01-09 14:26:48 -0700

    feat(core): add interface for Resource

--- packages/core/src/interfaces/index.ts
+++ packages/core/src/interfaces/index.ts
@@ -1,1 +1,18 @@
-export { };
+
+/**
+*   Resources are the common currency of the game.
+*   Food, Production, Gold, Faith, etc -- all are considered "Resources"
+*/
+export interface Resource {
+    /* name of resources */
+    name: string;
+    /* total that the player has */
+    total: number;
+    /* max the player can get */
+    max: number;
+    /* gain /s */
+    perSecond: number;
+    /* gain per click (if applicable) */
+    perClick: number;
+    description: string;
+}
