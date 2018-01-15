7dc8468a9b188d2c39d988ad8e1c59e3f2763de3
Emma Ramirez <ramirez.emma.g@gmail.com>
2018-01-15 12:08:29 -0700

    fix: remove externals

--- packages/app/webpack.config.js
+++ packages/app/webpack.config.js
@@ -1,3 +1,4 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const { baseConfig, COMMON_EXTERNALS } = require('../webpack-build-scripts');
 const path = require('path');
 
@@ -6,12 +7,17 @@ module.exports = {
 
     entry: './src/index.tsx',
 
-    externals: COMMON_EXTERNALS,
-
     output: {
         filename: 'bundle.js',
         path: path.join(__dirname, './dist'),
         publicPath: '/'
-    }
+    },
+
+    plugins: [
+        ...baseConfig.plugins,
+        new CopyWebpackPlugin([
+            { from: './src/index.html', to: '.' }
+        ])
+    ]
 
 };
