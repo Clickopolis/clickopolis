8efda7f00f52af2ec148c682f505c07e4066b533
Emma Ramirez <ramirez.emma.g@gmail.com>
2018-01-15 22:30:19 -0700

    chore: add constants file

---
+++ packages/app/src/utils/constants.ts
@@ -0,0 +1,18 @@
+export const CONSTANTS = Object.freeze({
+    screenData: [
+        'Resources',
+        'Civilization',
+        'Citizens',
+        'Economy',
+        'Buildings',
+        'Wonders',
+        'Technology',
+        'Diplomacy',
+        'Military',
+        'Culture',
+        'Faith',
+        'Legacy',
+        'Achievements',
+        'Settings'
+    ]
+});
