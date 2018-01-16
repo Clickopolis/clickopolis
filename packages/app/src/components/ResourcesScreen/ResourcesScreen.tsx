e63139d51cb47daca545436d25f9d886056bd946
Emma Ramirez <ramirez.emma.g@gmail.com>
2018-01-16 00:50:08 -0700

    fix: use ResourcesScreen from core

--- packages/app/src/components/ResourcesScreen/ResourcesScreen.tsx
+++ packages/app/src/components/ResourcesScreen/ResourcesScreen.tsx
@@ -1,38 +1,10 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
-
-// NOTE: Temporary Residence
+import { Screen } from '../../../../core';
 //@ts-ignore: no @types def
 import * as classNames from 'classnames';
 
 import './ResourcesScreen.scss';
-
-export interface ScreenProps {
-    className?: string | string[];
-    color: string;
-    style?: object;
-    type: string;
-}
-
-export class Screen extends React.Component<ScreenProps> {
-    constructor(props:ScreenProps) {
-        super(props);
-    }
-
-    public render() {
-        const { children, color, className, style, type } = this.props;
-
-        return (
-            <section style={{ ...style, borderTop: `4px solid ${color}`}} className={classNames(`${type.toLowerCase()}-screen`, className)} id={type}>
-                <header style={{ background: color }} className={classNames(`${type.toLowerCase()}-screen-header`, 'screen-header')}>
-                    <img alt={type} src={'./images/' + type.toLowerCase() + '.svg'} style={{ height: '2rem' }} />
-                    <h2>{ type }</h2>
-                </header>
-                { children }
-            </section>
-        );
-    }
-}
 
 export interface ResourcesScreenProps {
 
