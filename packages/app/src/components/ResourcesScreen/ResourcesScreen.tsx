51e5acbcb5a4cdb138146c615dbb7a2d88bd13c2
Emma Ramirez <ramirez.emma.g@gmail.com>
2018-01-16 17:46:31 -0700

    design: add Indicators, WIP

--- packages/app/src/components/ResourcesScreen/ResourcesScreen.tsx
+++ packages/app/src/components/ResourcesScreen/ResourcesScreen.tsx
@@ -1,27 +1,78 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
-import { Screen } from '../../../../core';
+import { Screen, Button, Indicator } from '../../../../core';
 //@ts-ignore: no @types def
 import * as classNames from 'classnames';
 
+import { colors } from 'utils';
+
 import './ResourcesScreen.scss';
+
+const indicatorStyle = {
+    height: '1.25rem',
+    width: '5rem'
+};
 
 export interface ResourcesScreenProps {
 
 }
 
-export class ResourcesScreenBase extends React.Component {
+export interface ResourcesScreenState {
+    foodTotal: number;
+}
+
+export class ResourcesScreenBase extends React.Component<ResourcesScreenProps, ResourcesScreenState> {
     constructor(props:ResourcesScreenProps) {
         super(props);
+        this.state = {
+            foodTotal: 10
+        };
     }
+
+    private growFood = e => this.setState({ foodTotal: this.state.foodTotal + 1 });
 
     public render() {
         return (
             <Screen
                 type='Resources'
-                color='#1edb50'
+                color={colors.get('resources')}
             >
-                <p></p>
+                <div className='resources-main-buttons'>
+                    <div className='resources-main-buttons-row'>
+                        <Button
+                            icon='./images/food.svg'
+                            id='food-btn'
+                            iconHeight='1rem'
+                            value='Grow Food'
+                            onClick={this.growFood}
+                            className='food-button'
+                            layout={null}
+                        />
+                        <Indicator
+                            value={this.state.foodTotal}
+                            positiveColor={colors.get('resources')}
+                            neutralColor={colors.get('resources')}
+                            style={indicatorStyle}
+                        />
+                    </div>
+                    <div className='resources-main-buttons-row'>
+                        <Button
+                            icon='./images/production.svg'
+                            id='production-btn'
+                            iconHeight='1rem'
+                            value='Create Production'
+                            onClick={this.growFood}
+                            className='prod-button'
+                            layout={null}
+                        />
+                        <Indicator
+                            value={this.state.foodTotal}
+                            positiveColor={colors.get('production')}
+                            neutralColor={colors.get('production')}
+                            style={indicatorStyle}
+                        />
+                    </div>
+                </div>
             </Screen>
         );
     }
--- packages/app/src/utils/colors.ts
+++ packages/app/src/utils/colors.ts
@@ -1,3 +1,4 @@
 export const colors = new Map([
+    ['production', '#e6d83e'],
     ['resources', '#1edb50']
 ]);
