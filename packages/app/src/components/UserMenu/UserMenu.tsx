407f840afdecf22a4971ada07f8e6ad6f210ff80
Emma Ramirez <ramirez.emma.g@gmail.com>
2018-01-18 00:41:20 -0700

    feat: add UserMenu component

---
+++ packages/app/src/components/UserMenu/UserMenu.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+
+export interface UserMenuProps {
+    username: string;
+    userCivName: string;
+}
+
+export class UserMenu extends React.Component<UserMenuProps> {
+    constructor(props:UserMenuProps) {
+        super(props);
+    }
+
+    public render() {
+        const { username, userCivName } = this.props;
+
+        return (
+            <div className='user-menu'>
+                <div className='user-info'>
+                    <div className='user-info-name'>{ username }</div>
+                    <div className='user-info-civ'>{ userCivName }</div>
+                </div>
+                <div className='user-profile'>
+                    <img src='' />
+                </div>
+            </div>
+        );
+    }
+}
