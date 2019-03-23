4ba2eb42e8c86e0f27ee7b83becb3a86dfd2ce61
EmmaRamirez <ramirez.emma.g@gmail.com>
2019-03-23 17:30:52 -0600

    fix: use process.env.PORT for server.ts

--- packages/app/server.ts
+++ packages/app/server.ts
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 app.use('/', express.static('./dist'));
 
-app.listen(3000, () => console.log('Running at port 3000'));
+app.listen(port, () => console.log(`Running at port ${port}`));
