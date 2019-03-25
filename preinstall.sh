fb112561a8b3a9004d7f709a6f5bc0a7bf5a9f90
EmmaRamirez <ramirez.emma.g@gmail.com>
2019-03-24 22:10:07 -0600

    chore: add bash preinstall script

---
+++ preinstall.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [[ -z "${CIRCLECI}" ]]; then
+    npm i -g ts-node lerna webpack@3 webpack-cli@3 &&\
+     lerna bootstrap &&\
+      cd ./packages/roadmap &&\
+       mkdir ./dist &&\
+        touch ./dist/index.html
+else
+    sudo npm i -g ts-node lerna webpack@3 webpack-cli@3 &&\
+     lerna bootstrap &&\
+      cd ./packages/roadmap &&\
+       sudo mkdir ./dist &&\
+        touch ./dist/index.html
+fi
