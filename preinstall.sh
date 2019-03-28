ed8fcb52adb4a9d72f566532a4173f42f04dd4f5
EmmaRamirez <ramirez.emma.g@gmail.com>
2019-03-28 11:37:39 -0600

    chore: update preinstall script

--- preinstall.sh
+++ preinstall.sh
@@ -2,12 +2,14 @@
 
 if [[ -z "${CIRCLECI}" ]]; then
     npm i -g ts-node lerna webpack@3 webpack-cli@3 &&\
+    lerna init &&\
      lerna bootstrap &&\
       cd ./packages/roadmap &&\
        mkdir ./dist &&\
         touch ./dist/index.html
 else
     sudo npm i -g ts-node lerna webpack@3 webpack-cli@3 &&\
+    sudo lerna init &&\
      lerna bootstrap &&\
       cd ./packages/roadmap &&\
        sudo mkdir ./dist &&\
