ddeb31dbc9f8073c3e929ebec2069688fed2e542
EmmaRamirez <ramirez.emma.g@gmail.com>
2019-03-28 00:12:25 -0600

    fix: update app/server.ts to use webpack

--- packages/app/server.ts
+++ packages/app/server.ts
@@ -1,8 +1,20 @@
+// const express = require('express');
+// const app = express();
+
+const port = process.env.PORT || 5000;
+
+// app.use('/', express.static('./dist'));
+
+// app.listen(port, () => console.log(`Running at port ${port}`));
+
+const webpack = require('webpack');
+const middleware = require('webpack-dev-middleware');
+const compiler = webpack(require('./webpack.config'));
 const express = require('express');
 const app = express();
 
-const port = process.env.PORT || 3000;
+app.use(middleware(compiler, {
+  // webpack-dev-middleware options
+}));
 
-app.use('/', express.static('./dist'));
-
-app.listen(port, () => console.log(`Running at port ${port}`));
+app.listen(port, () => console.log(`Running at port ${port}`))
