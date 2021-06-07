5e452c7a2d17b267bec4cde1eaa7114aeaac3b45
emmaramirez <ramirez.emma.g@gmail.com>
2021-06-07 01:04:06 -0600

    chore: add stub for contextual pointer

---
+++ packages/app/src/components/ContextualPointer/ContextualPointer.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export interface ContextualPointerProps {
+
+}
+
+export function ContextualPointer(props: ContextualPointerProps) {
+
+    return (
+        <div />
+    );
+}
