9e3a22ef05cdb7cb8eda499f6ea1de01da31f6b8
emmaramirez <ramirez.emma.g@gmail.com>
2021-06-08 20:44:35 -0600

    chore(deps): update deps

--- packages/app/src/components/BiomeIcon/BiomeIcon.tsx
+++ packages/app/src/components/BiomeIcon/BiomeIcon.tsx
@@ -1,6 +1,45 @@
 import * as React from 'react';
 import { Indicator, Biome } from '@clickopolis/core';
 import { colors } from 'utils';
+import { keyframes, stylesheet } from 'typestyle';
+
+const rotate = keyframes({
+    '0%': {
+        transform: 'rotate(0deg)',
+    },
+    '100%': {
+        transform: 'rotate(360deg)',
+    }
+});
+
+const css = stylesheet({
+    biomeIcon: {
+        position: 'relative',
+    },
+    biomeIconOverlay: {
+        height: '4.5rem',
+        width: '4.5rem',
+        position: 'absolute',
+        animationName: rotate,
+        animationIterationCount: 'infinite',
+        animationTimingFunction: 'ease-in-out',
+        animationDuration: '4s',
+        top: '-0.25rem',
+        zIndex: 1,
+    },
+    biomeIconIndicator: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '3.5rem',
+        width: '3.5rem',
+        margin: '.5rem',
+        //padding: '.25rem',
+        borderRadius: '50%',
+        cursor: 'pointer',
+        position: 'relative',
+    },
+});
 
 const biomeIconStyle = {
     //background: colors.get('biomes'),
@@ -9,14 +48,16 @@ const biomeIconStyle = {
     alignItems: 'center',
     height: '3.5rem',
     width: '3.5rem',
-    margin: '.25rem',
+    margin: '.5rem',
     //padding: '.25rem',
-    border: '4px solid transparent',
     borderRadius: '50%',
     cursor: 'pointer',
+    position: 'relative',
 }
 
-const isSelectedStyle = {border: `4px solid ${colors.get('biomes')}` };
+const isSelectedStyle = {
+    filter: `drop-shadow(0 0 3px gold)`,
+};
 
 export interface BiomeIconProps {
     biome: Biome;
@@ -24,20 +65,23 @@ export interface BiomeIconProps {
     onClick?: (e?: React.MouseEvent<HTMLElement>) => void;
 }
 
-export class BiomeIcon extends React.Component<BiomeIconProps> {
+export class BiomeIcon extends React.PureComponent<BiomeIconProps> {
 
     public render() {
-        const {biome, onClick, isSelected} = this.props
+        const {biome, onClick, isSelected} = this.props;
 
-        return <Indicator
-            style={{
-                ...(isSelected && isSelectedStyle),
-                ...biomeIconStyle,
-            }}
-            iconHeight='3.5rem'
-            icon={`./images/${biome.name}.svg`}
-            description={biome.name}
-            onClick={onClick}
-        />
+        return <div className={css.biomeIcon}>
+            {isSelected && <img className={css.biomeIconOverlay} src='./images/biome-overlay.svg' />}
+            <Indicator
+                style={{
+                    ...(isSelected && isSelectedStyle),
+                    ...biomeIconStyle,
+                }}
+                iconHeight='3.5rem'
+                icon={`./images/${biome.name}.svg`}
+                description={biome.name}
+                onClick={onClick}
+            />
+        </div>;
     }
 }
