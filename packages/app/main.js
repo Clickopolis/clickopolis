43c02d95905786e7bc95209cc1859db573039baf
emmaramirez <ramirez.emma.g@gmail.com>
2021-06-17 01:19:54 -0600

    refactor: src -> src_old

--- packages/app/main.js
+++ packages/app/main.js
@@ -1,8 +1,4 @@
 const { app, BrowserWindow } = require('electron');
-const {
-  default: installExtension,
-  REDUX_DEVTOOLS,
-} = require('electron-devtools-installer');
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -15,10 +11,6 @@ function createWindow() {
 
 app.whenReady().then(() => {
   createWindow();
-
-  installExtension(REDUX_DEVTOOLS)
-    .then((name) => console.log(`Added Extension:  ${name}`))
-    .catch((err) => console.log('An error occurred: ', err));
 
   app.on('activate', function () {
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
